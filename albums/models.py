a88a9929af93e94312f1a91d5eb2ef819a9ddd04
Sinanaag <sinanaag@gmail.com>
2026-03-16 22:42:24 +0530

    Add project files

---
+++ albums/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+#database models for albums and photos 
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    event_date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
